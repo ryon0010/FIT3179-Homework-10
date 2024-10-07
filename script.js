63798acc2181528f646192f2a4d065533bac7ba5
ryon0010 <ryon0010@student.monash.edu>
2024-10-07 11:01:26 +0800

    created 3 graphs

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var vg_1 = "map.json";
-vegaEmbed("#map", vg_1).then(function(result) {
-// Access the Vega view instance  (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
-
-
-var vg_1 = "./js/UK_symbolmap_with_graticules.json";
-vegaEmbed("#symbol_map", vg_1, {renderer: "svg", actions: false})
+vegaEmbed("#map", vg_1, {renderer: "svg", actions: false})
     .then(function (result) { })
     .catch(console.error);
 
-var vg_2 = "./js/histogram.json";
-vegaEmbed("#histo", vg_2, {renderer: "svg", actions: false})
+var vg_2 = "line.json";
+vegaEmbed("#line", vg_2, {renderer: "svg", actions: false})
     .then(function (result) { })
     .catch(console.error);
+
+var vg_3 = "mean.json";
+vegaEmbed("#mean", vg_3, {renderer: "svg", actions: false})
+    .then(function (result) { })
+    .catch(console.error);
+    // var spec2 = "js/scatter_plot_interactive.vg.json";
+    // vegaEmbed('#scatter_plot', spec2).then(function(result) {
+    //   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    // }).catch(console.error);
