ecef626733bb8339abf623d0860f38052e360b75
ryon0010 <ryon0010@student.monash.edu>
2024-10-08 11:03:20 +0800

    changes made

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-var vg_1 = "map.json";
-vegaEmbed("#map", vg_1, {renderer: "svg", actions: false})
-    .then(function (result) { })
+var vg_1 = "map1_vis.json";
+vegaEmbed("#map1", vg_1)
+    .then(function (result) {})
     .catch(console.error);
 
-var vg_2 = "line.json";
-vegaEmbed("#line", vg_2, {renderer: "svg", actions: false})
-    .then(function (result) { })
-    .catch(console.error);
-
-var vg_3 = "mean.json";
-vegaEmbed("#mean", vg_3, {renderer: "svg", actions: false})
-    .then(function (result) { })
-    .catch(console.error);
-    // var spec2 = "js/scatter_plot_interactive.vg.json";
-    // vegaEmbed('#scatter_plot', spec2).then(function(result) {
-    //   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-    // }).catch(console.error);
